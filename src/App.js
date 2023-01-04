7e477c6e81f590bab4ab2d2280c793e2ea7f0ff2
Cristian Moyano <cristianmoyano87@hotmail.com>
2023-01-04 10:11:39 -0300

    agregado localStorage dia 9

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    if(newRegalo!==''){
+      if(!regalos.some((element)=>{return element.desc===newRegalo}))
+      setRegalos([...regalos, {desc:newRegalo, Qty:newQty}])
+      setNewRegalo('')
+      setNewQty(1)
+    }
+  }
+  
+  // Carga inicial de regalos desde localStorage cuando se inicia el componente
+  const [regalos, setRegalos] = useState(JSON.parse(localStorage.getItem('giftsList')) || [{desc:'Medias',Qty:1}, {desc:'Caramelos', Qty:1}, {desc:'Vittel Tone', Qty:1}])
+  const [newRegalo, setNewRegalo] = useState('')
+  const [newQty, setNewQty] = useState(1)
+
+  const handleChange = (evt) => { setNewRegalo(evt.target.value) }
+  const handleChangeQty = (evt) => { setNewQty(evt.target.value) }
+  const handleDelete = (borrar) => {
+    const result = regalos.filter(regalo => regalo !== borrar)
+    setRegalos(result)
+  }
+
+  // Actualiza localStorage cuando cambia "regalos"
+  useEffect(()=>{
+    localStorage.setItem('giftsList', JSON.stringify(regalos))
+  },[regalos])
+
+  const ListaRegalos = () => {
+    if(regalos.length===0) {
+      return <h3>No hay regalos, suma uno</h3>
+    }
+    return <>
+    <ul>
+      {regalos.map(regalo => 
+        <li key={regalo.desc}>
+          {regalo.desc} {regalo.Qty>1?"x"+regalo.Qty:""}
+          <button onClick={() => handleDelete(regalo)}>
+            <img alt="trash" src="//ssl.gstatic.com/ui/v1/icons/mail/gm3/1x/delete_baseline_nv700_20dp.png"/>
+          </button>
+        </li>
+      )}    
+    </ul>
+    </>
+  }
+
+  const BorrarLista = () => {
+    return <button className="borrarTodo" onClick={() => handleBorrarLista()}>
+      <img alt="trash" src="//ssl.gstatic.com/ui/v1/icons/mail/gm3/1x/delete_baseline_nv700_20dp.png"/>
+      Borrar todo
+    </button>
+  }
+
+  const handleBorrarLista = () => {
+    setRegalos([])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className='fondo' src='cuento-arbol-navidad.jpg' alt='fondo'/>
+        <p className="titulo">Regalos</p>
+        <form onSubmit={handleSubmit}>
+          <input id="descripcion" onChange={handleChange} value={newRegalo}/>
+          <input id="cantidad" onChange={handleChangeQty} value={newQty} type="number" min={1} size={3} />
+          <button>Agregar</button>
+        </form>
+        {/* {regalos.map(regalo => <p>{regalo}<button onClick={() => handleDelete(regalo)}><img alt="recycle" src="//ssl.gstatic.com/ui/v1/icons/mail/gm3/1x/delete_baseline_nv700_20dp.png"/></button></p>)} */}
+        <ListaRegalos/>
+        <BorrarLista/>
       </header>
     </div>
   );
